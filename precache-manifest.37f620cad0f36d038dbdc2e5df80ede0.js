43e7a9bf192ac4b05e97cd7d0a9ab39e40f18f30
GodfreySam <godfreysam09@gmail.com>
2020-10-04 22:01:02 +0100

    Updates

---
+++ precache-manifest.37f620cad0f36d038dbdc2e5df80ede0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453c0eb9d92306d55be60be83ef219c7",
+    "url": "/react-calculator-app/index.html"
+  },
+  {
+    "revision": "e6450306714589372daf",
+    "url": "/react-calculator-app/static/css/main.c4905a20.chunk.css"
+  },
+  {
+    "revision": "72f338748bb8a966e487",
+    "url": "/react-calculator-app/static/js/2.5beaf1a6.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/react-calculator-app/static/js/2.5beaf1a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6450306714589372daf",
+    "url": "/react-calculator-app/static/js/main.5e8fcf55.chunk.js"
+  },
+  {
+    "revision": "1835b1a1acfbdca2d7c3",
+    "url": "/react-calculator-app/static/js/runtime-main.4c732c0d.js"
+  }
+]);
